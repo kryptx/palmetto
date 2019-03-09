a93139e59aba1221802d9b8f10ff0772c714e4ff
Jason Burbage <kryptx@gmail.com>
2019-03-08 23:23:01 -0700

    Rewrite to new spec as it's evolving.
    Finish implementing middleware.
    Clean up some routes.

--- sample-client/authorization/middleware.js
+++ sample-client/authorization/middleware.js
@@ -3,9 +3,16 @@ const { parseHeader } = require('palmetto');
 
 module.exports = async (req, res, next) => {
   const palmetto = parseHeader(req.headers.authorization);
+
   if(palmetto) {
+    const srvResult = await lookupSRV(`_palmetto._tcp.${palmetto.domain}`);
+    const pip = srvResult[0]; // todo: use priority and weight
+    palmetto.url = `https://${pip.name}:${pip.port}${palmetto.path}`;
     palmetto.state = await randomBytes(64).toString('hex');
     req.session.auth = { palmetto };
+    res.set('Location', `${palmetto.url}/authorize`);
+    res.status(302).send();
   }
-  // TODO: redirect to authorization endpoint
+
+  return next();
 }
--- sample-client/routes/callback.js
+++ sample-client/routes/callback.js
@@ -1,48 +1,33 @@
 'use strict';
 
-const Url = require('url');
 const Request = require('superagent');
-const { boomify, unauthorized } = require('boom');
+const { unauthorized } = require('boom');
+const get = require('lodash.get');
 
 module.exports = exports = {
   path: '/palmetto/callback',
   method: 'get',
-  handle: state => [
-    (req, res, next) => {
+  handle: svc => [
+    async (req, res, next) => {
       // send 401 if they wandered in here without a hall pass
-      if(!req.params.authorization_code) return next(unauthorized('You must provide an authorization code.'));
+      if(!req.query.authorization_code) return next(unauthorized('You must provide an authorization code.'));
 
       // send 401 if it doesn't look like they have the right stuff in the session
-      // TODO: validate properly
-      if(!req.session.auth            ||
-         !req.session.auth.palmettoId ||
-         !req.session.auth.state      ||
-         !req.params.state            ||
-          req.session.auth.state !== req.params.state) {
-            return next(unauthorized('Something went wrong. Please retry the authorization flow from the beginning.'))
+      if(get(req.session, 'auth.palmetto.state') !== req.query.state) {
+        return next(unauthorized('Something went wrong. Please retry the authorization flow from the beginning.'))
       }
 
-      let authParts = req.headers.authorization.split(' ');
-      let id = authParts[1];
-      if(id !== res.locals.palmettoId) return next(unauthorized('Session ID mismatched authorization header.'));
-
-      try {
-        res.locals.palmettoId = Url.parse(id);
-      } catch (typeErrorProbably) {
-        return next(boomify(typeErrorProbably, { statusCode: 401 } ));
-      }
-    },
-    async (req, res, next) => {
       let response = await Request
-        .post(res.locals.palmettoId)
+        .post(req.session.auth.palmetto.url)
         .send({ authorization_code: req.params.authorization_code })
 
       if(response.ok) {
         req.session.user = response;
+        delete req.session.auth;
         return next();
       }
 
-      return next(unauthorized('Well. You said no.'))
+      return next(unauthorized('What did you expect? You said no.'))
     }
   ]
 }
